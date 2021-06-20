42df69cea683ae97c732ecc5e1cd62b9931b9a2d
Christian Lumapac <christianlumapac@gmail.com>
2021-06-20 21:30:32 +0800

    Update content view

--- app/src/main/java/com/jetpack/compose/cartrack/MainActivity.kt
+++ app/src/main/java/com/jetpack/compose/cartrack/MainActivity.kt
@@ -3,21 +3,43 @@ package com.jetpack.compose.cartrack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
-import com.jetpack.compose.cartrack.ui.theme.CartrackTheme
+import androidx.compose.ui.unit.dp
+import com.jetpack.compose.cartrack.ui.theme.CarTrackTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CartrackTheme {
+            CarTrackTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Column(
+                        Modifier
+                            .fillMaxSize()
+                            .wrapContentSize(Alignment.Center)
+                    ) {
+                        UsernameInputField(
+                            value = "",
+                            onInput = {},
+                            modifier = Modifier.padding(8.dp)
+                        )
+                        PasswordInputField(
+                            value = TextFieldValue(),
+                            onInput = {},
+                            label = { Text("Foo") },
+                            modifier = Modifier.padding(8.dp)
+                        )
+                        RememberMe()
+                    }
                 }
             }
         }

bdcca27fc6953d94b0efd4f8e2543216457d3c1e
Christian Lumapac <christianlumapac@gmail.com>
2021-06-21 01:54:24 +0800

    Update Api.kt

--- app/src/main/java/com/jetpack/compose/cartrack/entities/Api.kt
+++ app/src/main/java/com/jetpack/compose/cartrack/entities/Api.kt
@@ -2,9 +2,8 @@ package com.jetpack.compose.cartrack.entities
 
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 interface Api {
     @GET("users")
-    fun getRepository(): Single<Repository>
+    fun getRepository(): Single<CarTrackUsers>
 }

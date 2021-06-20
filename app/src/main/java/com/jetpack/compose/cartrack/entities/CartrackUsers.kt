b0f51617840bf12763301a9ea0fc5a5295ead3b1
Christian Lumapac <christianlumapac@gmail.com>
2021-06-21 01:54:34 +0800

    Create CartrackUsers.kt

---
+++ app/src/main/java/com/jetpack/compose/cartrack/entities/CartrackUsers.kt
@@ -0,0 +1,17 @@
+package com.jetpack.compose.cartrack.entities
+
+data class CarTrackUsers(
+    val id: Int?, val name: String?, val username: String?,
+    val email: String?, val address: Address?,
+    val phone: String?, val website: String?,
+    val company: Company?
+)
+
+data class Address(
+    val street: String?, val suite: String?,
+    val city: String?, val zipCode: String?, val geo: Geo
+)
+
+data class Company(val name: String?, val catchPhrase: String?, val bs: String?)
+
+data class Geo(val lat: String?, val lng: String?)

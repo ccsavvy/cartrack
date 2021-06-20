9f8e7681fe8c6cd0b2fcd5dfb8696de3cebc1a3b
Christian Lumapac <christianlumapac@gmail.com>
2021-06-20 21:51:12 +0800

    Add to use use Icons.Filled.Visibility and Icons.Filled.VisibilityOff

--- app/src/main/java/com/jetpack/compose/cartrack/MainActivity.kt
+++ app/src/main/java/com/jetpack/compose/cartrack/MainActivity.kt
@@ -6,11 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jetpack.compose.cartrack.ui.theme.CartrackTheme
@@ -47,11 +53,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Composable
 fun UsernameInputField(
     value: String,
     onInput: (String) -> Unit,
@@ -77,12 +78,27 @@ fun PasswordInputField(
     label: (@Composable () -> Unit)? = null,
     placeholder: (@Composable () -> Unit)? = null,
 ) {
-    var passwordVisible by remember { mutableStateOf(false) }
+
     var textValue by remember { mutableStateOf(value) }
+    var passwordVisibility by remember { mutableStateOf(false) }
+
     TextField(
         value = textValue,
         onValueChange = { onInput(it) },
         label = { Text(stringResource(id = R.string.password)) },
+        visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            val image = if (passwordVisibility)
+                Icons.Filled.Visibility
+            else Icons.Filled.VisibilityOff
+
+            IconButton(onClick = {
+                passwordVisibility = !passwordVisibility
+            }) {
+                Icon(imageVector = image, "")
+            }
+        },
         modifier = modifier
     )
 }
